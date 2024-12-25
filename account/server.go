fc32f9581ec06375e903edbb059872cee2353cc3
Anideep-Kalia <anideepkalia@gmail.com>
2024-12-25 23:21:27 +0530

    server is done

---
+++ account/server.go
@@ -0,0 +1,40 @@
+// Send data to the client; receive data from the client; send data back to client in form of GRPC response
+package account
+
+import (
+	"context"
+
+	// to generate unique ids
+	"github.com/segmentio/ksuid"
+)
+
+
+type accountService struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &accountService{r}
+}
+
+func (s *accountService) PostAccount(ctx context.Context, name string) (*Account, error) {
+	a := &Account{
+		Name: name,
+		ID:   ksuid.New().String(),
+	}
+	if err := s.repository.PutAccount(ctx, *a); err != nil {
+		return nil, err
+	}
+	return a, nil
+}
+
+func (s *accountService) GetAccount(ctx context.Context, id string) (*Account, error) {
+	return s.repository.GetAccountByID(ctx, id)
+}
+
+func (s *accountService) GetAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error) {
+	if take > 100 || (skip == 0 && take == 0) {
+		take = 100
+	}
+	return s.repository.ListAccounts(ctx, skip, take)
+}
