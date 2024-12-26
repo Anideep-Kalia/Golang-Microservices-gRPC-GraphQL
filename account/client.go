4013df96ec3d36031ab79e32d995eaf8ce5e4206
Anideep-Kalia <anideepkalia@gmail.com>
2024-12-26 06:12:14 +0530

    client.go checked

--- account/client.go
+++ account/client.go
@@ -9,16 +9,16 @@ import (
 )
 
 type Client struct {
-	conn    *grpc.ClientConn
-	service pb.AccountServiceClient
+	conn    *grpc.ClientConn								// reference to the gRPC connection
+	service pb.AccountServiceClient							// reference to the gRPC client
 }
 
 func NewClient(url string) (*Client, error) {
-	conn, err := grpc.Dial(url, grpc.WithInsecure())
+	conn, err := grpc.Dial(url, grpc.WithInsecure())		// Insecure is used stating that the connection is not secure i.e. localhost 8080
 	if err != nil {
 		return nil, err
 	}
-	c := pb.NewAccountServiceClient(conn)
+	c := pb.NewAccountServiceClient(conn)					// new instance of the gRPC client
 	return &Client{conn, c}, nil
 }
 
@@ -29,12 +29,12 @@ func (c *Client) Close() {
 func (c *Client) PostAccount(ctx context.Context, name string) (*Account, error) {
 	r, err := c.service.PostAccount(
 		ctx,
-		&pb.PostAccountRequest{Name: name},
+		&pb.PostAccountRequest{Name: name},			// triggering the PostAccount function in the server.go file
 	)
 	if err != nil {
 		return nil, err
 	}
-	return &Account{
+	return &Account{								// returing response in the form of Account struct
 		ID:   r.Account.Id,
 		Name: r.Account.Name,
 	}, nil
